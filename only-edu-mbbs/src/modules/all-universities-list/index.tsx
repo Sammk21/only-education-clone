32855173ed4f38451c14020bbff9db4d33a2c176
onlyeducationpvt <sarfrajonlyeducation@gmail.com>
2024-06-21 11:27:36 +0530

    some part change of filter

--- only-edu-mbbs/src/modules/all-universities-list/index.tsx
+++ only-edu-mbbs/src/modules/all-universities-list/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CollegeFilter from "./college-filter";
 import CollegeList from "./college-list";
+import { getStrapiData } from "@/utils/utils";
 import { ImageAttributes } from "@/types/types";
 
 interface Ownership {
@@ -63,8 +64,6 @@ interface Props {
   exams: {
     data: Option[];
   };
-
-  filterData: UniversitiesData;
   data: UniversitiesData;
 }
 
@@ -73,16 +72,33 @@ const AllUniversitiesList = ({
   indianStates,
   exams,
   data,
-  filterData,
-}: //   filterData,
-Props) => {
+}: Props) => {
   const [selectedFilters, setSelectedFilters] = useState({});
+  const [filteredData, setFilteredData] = useState<UniversitiesData>(data);
 
-  const getData = (updatedAttributes: object) => {
+  const getData = async (updatedAttributes: object) => {
     setSelectedFilters(updatedAttributes);
+
+    // Construct dynamic filter query based on updatedAttributes
+    let filterQuery = "/api/universities?";
+    const queryParams = Object.entries(updatedAttributes)
+      .map(([key, value]) => `filters[${key}][slug][$eq]=${value}`)
+      .join("&");
+
+    filterQuery += `${queryParams}&populate[universityProfile][populate][backgroundImage][populate][0]=universityProfile.backgroundImage&populate[streams][populate]=true&populate[indian_state][populate]=true&populate[ownership][populate]=true&populate[exams][populate]=true`;
+
+    // Fetch new data based on the filter query
+    const newFilterData = await getStrapiData(filterQuery);
+    setFilteredData(newFilterData);
   };
 
-  console.log(selectedFilters);
+  useEffect(() => {
+    // This effect will run whenever selectedFilters changes
+    if (Object.keys(selectedFilters).length > 0) {
+      getData(selectedFilters);
+    }
+  }, [selectedFilters]);
+
   return (
     <div className="flex flex-col lg:flex-row justify-center">
       <CollegeFilter
@@ -91,7 +107,7 @@ Props) => {
         indianStates={indianStates}
         setFun={getData}
       />
-      <CollegeList data={data} filterData={filterData} />
+      <CollegeList data={filteredData} />
     </div>
   );
 };
