bbca8167c667009c4b59bf5520ad9549664734e5
onlyeducationpvt <sarfrajonlyeducation@gmail.com>
2024-07-01 18:59:45 +0530

    newsletter modified

--- only-edu-mbbs/src/modules/newsletter/index.tsx
+++ only-edu-mbbs/src/modules/newsletter/index.tsx
@@ -39,14 +39,14 @@ const NewsLetter: React.FC = () => {
   };
 
   return (
-    <div className="relative isolate overflow-hidden bg-greenDark pb-16 my-8 sm:py-24 lg:py-32 mx-3 max-w-7xl lg:mx-auto rounded-3xl">
+    <div className="relative isolate overflow-hidden text-dark bg-gradient-to-l from-[#f49e43] to-[#fbdcad] pb-16 my-8 sm:py-24 lg:py-20 mx-3 max-w-7xl lg:mx-auto rounded-3xl">
       <div className="mx-auto px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl pt-6 grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
           <div className="max-w-xl lg:max-w-lg">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+            <h2 className="text-3xl font-bold tracking-tight text-dark sm:text-4xl">
               Subscribe to our newsletter.
             </h2>
-            <p className="mt-4 text-lg leading-8 text-gray-300 hidden md:block">
+            <p className="mt-4 text-md leading-8 text-dark hidden md:block">
               Get Latest Notification Of Colleges, Exams and News
             </p>
             <form
@@ -64,12 +64,12 @@ const NewsLetter: React.FC = () => {
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                className="min-w-0 flex-auto rounded-md  bg-white/80 px-3.5 py-2 text-dark shadow-sm ring-1 ring-inset ring-white/10    sm:text-sm sm:leading-6  border"
                 placeholder="Enter your email"
               />
               <button
                 type="submit"
-                className="flex-none rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex-none rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-light shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
               >
                 Subscribe
               </button>
@@ -78,26 +78,26 @@ const NewsLetter: React.FC = () => {
           <div className="hidden md:block">
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
               <div className="flex flex-col items-start">
-                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                <div className="rounded-md bg-white/20 p-2 ring-1 ring-white/10">
                   <BsCalendar
-                    className="h-6 w-6 text-white"
+                    className="h-6 w-6 text-dark"
                     aria-hidden="true"
                   />
                 </div>
-                <dt className="mt-4 font-semibold text-white">
+                <dt className="mt-4 font-semibold text-dark">
                   Weekly articles
                 </dt>
-                <dd className="mt-2 leading-7 text-gray-400">
+                <dd className="mt-2 leading-7 text-dark">
                   Stay informed and engaged with our thought-provoking content
                   delivered straight to you.
                 </dd>
               </div>
               <div className="flex flex-col items-start">
-                <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                  <HandIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                <div className="rounded-md bg-white/20 p-2 ring-1 ring-white/10">
+                  <HandIcon className="h-6 w-6 text-dark" aria-hidden="true" />
                 </div>
-                <dt className="mt-4 font-semibold text-white">No spam</dt>
-                <dd className="mt-2 leading-7 text-gray-400">
+                <dt className="mt-4 font-semibold text-dark">No spam</dt>
+                <dd className="mt-2 leading-7 text-dark">
                   We value your inbox, ensuring no spam—just relevant updates
                   and information tailored to your interests.
                 </dd>
