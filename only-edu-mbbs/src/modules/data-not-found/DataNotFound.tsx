9b0ae259e26e0e38af5d8224896a8280211d9c0d
Sam <05sameerk@gmail.com>
2024-08-08 16:36:23 +0530

    data not founf component

---
+++ only-edu-mbbs/src/modules/data-not-found/DataNotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataNotFound = () => {
+  return (
+    <div className="h-[100dvh] w-[100dvh] flex justify-center items-center text-black text-3xl">
+      <h4>Data not found we are still working on correct our data</h4>
+    </div>
+  );
+};
+
+export default DataNotFound;
